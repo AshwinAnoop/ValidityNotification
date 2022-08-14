ec60707288f6f9966ac480971295e0158f022455
AshwinAnoop <ashwinka999@gmail.com>
2022-08-15 00:33:33 +0530

    login and register completed

--- vns/views.py
+++ vns/views.py
@@ -1,7 +1,53 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 
 # Create your views here.
-
 def index(request):
     return render(request,'index.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password'] 
+
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('home')
+        else:
+            messages.info(request,'Invalid Credentials')
+            return render(request,'login.html')
+
+    else:
+        return render(request,'login.html')
+
+def register(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirmpassword = request.POST['confirmpassword']
+
+        if password == confirmpassword:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,'Username already taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'Email already taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username,password=password,email=email)
+                user.save()
+                print("user created")
+        else:
+            messages.info(request,'password not matching')
+            return redirect('register')
+    else:
+        return render(request,'register.html')
+    return redirect('/')
+
+def home(request):
+    return render(request,'home.html')
+
